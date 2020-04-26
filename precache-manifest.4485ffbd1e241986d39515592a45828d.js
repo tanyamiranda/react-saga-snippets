9f2959ebf59660fcc6f51f4324aca29fa33bab06
tanya <60669157+tanyamiranda@users.noreply.github.com>
2020-04-26 11:13:48 -0400

    Updates

---
+++ precache-manifest.4485ffbd1e241986d39515592a45828d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86783ed846ab418c9efdadcc0c18d48d",
+    "url": "/redux-saga-snippets/index.html"
+  },
+  {
+    "revision": "f2370fc4626f6771d725",
+    "url": "/redux-saga-snippets/static/css/main.c9b28d2f.chunk.css"
+  },
+  {
+    "revision": "535bad0feef1cc1aebf4",
+    "url": "/redux-saga-snippets/static/js/2.c2dca1e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-saga-snippets/static/js/2.c2dca1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2370fc4626f6771d725",
+    "url": "/redux-saga-snippets/static/js/main.0db3fdf5.chunk.js"
+  },
+  {
+    "revision": "91ee19f793dc46123d58",
+    "url": "/redux-saga-snippets/static/js/runtime-main.96d16925.js"
+  }
+]);

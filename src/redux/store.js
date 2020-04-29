9cb9636dce79c59d893360c1f98d1e8fae437b0c
tanya <60669157+tanyamiranda@users.noreply.github.com>
2020-04-29 10:42:56 -0400

    Removed persist as it is not needed here.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
-import {persistStore} from 'redux-persist';
 
 import rootReducer from './root-reducer';
 import rootSaga from './root-saga';
@@ -14,6 +13,4 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSaga); 
 
-export const persistor = persistStore(store);
-
-export default {store, persistor};
+export default {store};
